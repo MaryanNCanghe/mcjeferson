2c3d14aae8a5a812da001b563813784b46a33081
Kiki <132509932+MaryanNCanghe@users.noreply.github.com>
2024-06-27 11:39:28 +0200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,16 +7,16 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="social-icons">
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.facebook.com/Gessou01" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faFacebook} />
         </a>
         <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faTwitter} />
         </a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/mcjefynhoorvoficial__/" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faInstagram} />
         </a>
-        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://youtube.com/@melhorgestaohits1?si=ZF7EeXvKWa_snL52" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faYoutube} />
         </a>
     
